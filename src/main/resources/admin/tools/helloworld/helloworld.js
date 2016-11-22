320f655091b2e3954ffbb20b4c8700862f620632
Glenn Ricaud <gri@enonic.com>
2016-11-22 13:16:25 +0100

    EXT-305 Starter kit - Admin tool

---
+++ src/main/resources/admin/tools/helloworld/helloworld.js
@@ -0,0 +1,13 @@
+var portalLib = require('/lib/xp/portal');
+var thymeleaf = require('/lib/xp/thymeleaf');
+
+exports.get = function (req) {
+    var view = resolve('./helloworld.html');
+    var params = {
+        adminUiAssetsUrl: portalLib.assetUrl({path: "", application: "com.enonic.xp.admin.ui"}),
+        launcherJsUrl: portalLib.assetUrl({path: "/js/launcher.js", application: "com.enonic.xp.admin.ui"})
+    };
+    return {
+        body: thymeleaf.render(view, params)
+    };
+};
