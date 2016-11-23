f5af326a5800f46619dd3d393694c109eddb37fa
Glenn Ricaud <gri@enonic.com>
2016-11-23 09:05:48 +0100

    EXT-305 Starter kit - Admin tool

--- src/main/resources/admin/tools/toolstarter/toolstarter.js
+++ src/main/resources/admin/tools/toolstarter/toolstarter.js
@@ -4,10 +4,13 @@ var mustacheLib = require('/lib/xp/mustache');
 exports.get = function (req) {
     var view = resolve('toolstarter.html');
 
+    // Variables required by the Launcher Panel
     var xpVersion = Java.type("com.enonic.starter.admintool.VersionInfoSupplier").get();
+    var adminAssetsUrl = portalLib.url({path: "/admin/assets/" + xpVersion});
+    var assetsUrl = portalLib.assetUrl({path: ""});
     var params = {
-        adminAssetsUrl: portalLib.url({path: "/admin/assets/" + xpVersion}),
-        assetsUrl: portalLib.assetUrl({path: ""})
+        adminAssetsUrl: adminAssetsUrl,
+        assetsUrl: assetsUrl
     };
 
     return {
