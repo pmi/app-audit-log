1a516880a03a9f4fd6deaf6d6764bfbc6661c5ae
Glenn Ricaud <gri@enonic.com>
2016-11-23 11:08:25 +0100

    EXT-305 Starter kit - Admin tool

--- src/main/resources/admin/tools/toolstarter/toolstarter.js
+++ src/main/resources/admin/tools/toolstarter/toolstarter.js
@@ -6,9 +6,11 @@ exports.get = function (req) {
     var view = resolve('toolstarter.html');
 
     // Variables required by the Launcher Panel
+    var adminUrl = portalLib.url({path: '/admin'})
     var adminAssetsUrl = portalLib.url({path: "/admin/assets/" + xpVersion});
     var assetsUrl = portalLib.assetUrl({path: ""});
     var params = {
+        adminUrl: adminUrl,
         adminAssetsUrl: adminAssetsUrl,
         assetsUrl: assetsUrl
     };
