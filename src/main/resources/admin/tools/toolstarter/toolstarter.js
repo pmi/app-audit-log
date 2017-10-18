7b387b3d6e4b814b828bd5e5f44ea6edc05d44a9
Alan Semenov <ase@enonic.com>
2017-10-18 13:47:06 +0200

    Fixed paths to admin assets and the launcher. Included lib-admin-ui in the build.

--- src/main/resources/admin/tools/toolstarter/toolstarter.js
+++ src/main/resources/admin/tools/toolstarter/toolstarter.js
@@ -1,18 +1,26 @@
 var portalLib = require('/lib/xp/portal');
 var mustacheLib = require('/lib/xp/mustache');
-var xpVersion = require('/lib/status').getXpVersion();
+var adminLib = require('/lib/xp/admin');
 
 exports.get = function (req) {
     var view = resolve('toolstarter.html');
 
     // Variables required by the Launcher Panel
-    var adminUrl = portalLib.url({path: '/admin'})
-    var adminAssetsUrl = portalLib.url({path: "/admin/assets/" + xpVersion});
+    var baseHref = portalLib.pageUrl({
+        path: '',
+        type: 'absolute'
+    });
+    
+    var adminUrl = adminLib.getBaseUri();
     var assetsUrl = portalLib.assetUrl({path: ""});
     var params = {
+        baseHref: baseHref,
         adminUrl: adminUrl,
-        adminAssetsUrl: adminAssetsUrl,
-        assetsUrl: assetsUrl
+        assetsUrl: assetsUrl,
+        launcherUrl: portalLib.assetUrl({
+            path: '/js/launcher',
+            application: 'com.enonic.xp.app.main'
+        })
     };
 
     return {
